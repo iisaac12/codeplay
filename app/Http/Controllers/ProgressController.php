ed6f3c69770b16c13dda91da5c7a44c7409f1425
iisaac12 <immanuelissac03@gmail.com>
2025-11-24 18:10:14 +0700

    controllers

---
+++ app/Http/Controllers/ProgressController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\UserEnrollment;
+use App\Models\MaterialProgress;
+use App\Models\QuizAttempt;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProgressController extends Controller
+{
+    public function index()
+    {
+        $enrollments = UserEnrollment::where('user_id', Auth::id())
+            ->with('course')
+            ->orderBy('enrolled_at', 'desc')
+            ->get();
+
+        $totalQuizzes = QuizAttempt::where('user_id', Auth::id())->count();
+        $passedQuizzes = QuizAttempt::where('user_id', Auth::id())
+            ->where('is_passed', true)
+            ->count();
+
+        $totalMaterials = MaterialProgress::where('user_id', Auth::id())->count();
+        $completedMaterials = MaterialProgress::where('user_id', Auth::id())
+            ->where('is_completed', true)
+            ->count();
+
+        return view('progress.index', compact(
+            'enrollments',
+            'totalQuizzes',
+            'passedQuizzes',
+            'totalMaterials',
+            'completedMaterials'
+        ));
+    }
+}
